d2e0038a08dadb628875b52e7e57001fbe9ae0e5
Austin <austincumberlander@gmail.com>
2019-03-09 13:56:02 -0600

    need to work on logic more.

--- JosephusProblem/Program.cs
+++ JosephusProblem/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JosephusProblem
 {
@@ -6,7 +8,49 @@ namespace JosephusProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void LastPersonStanding(int numberOfPeople, int killingInterval)
+            {
+                IEnumerable<int> numRange = Enumerable.Range(1, numberOfPeople);
+
+                Console.WriteLine("The Beginning");
+                Console.WriteLine($"we are starting with {numberOfPeople} people.");
+                Console.WriteLine($"The killing interval is {killingInterval}.");
+                Console.WriteLine("___________________");
+                Console.WriteLine("");
+
+                foreach (int num in numRange)
+                {
+                    Console.WriteLine(num);
+                }
+
+                List<int> rangeList = numRange.ToList();
+
+                Console.WriteLine("");
+                int peopleLeft = rangeList.Count;
+                var indexOfInterval = rangeList[killingInterval] - 2;
+
+                while (peopleLeft >= killingInterval)
+                {
+                    var roundNum = 0;
+                    roundNum++;
+
+                    rangeList.RemoveAt(indexOfInterval);
+
+                    Console.WriteLine($"Round {roundNum}");
+                    Console.WriteLine("___________________");
+                    Console.WriteLine("");
+
+                    foreach (int num in rangeList)
+                    {
+                        Console.WriteLine(num);
+                    }
+                    Console.WriteLine($"There are only {peopleLeft} people left.");
+                    Console.ReadLine();
+                }
+                    Console.ReadLine();
+            }
+
+            LastPersonStanding(10, 3);
         }
     }
 }
